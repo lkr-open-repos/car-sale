eb7d8a77624c2fa749b3a64f9014b67be8d9b3f7
lkrbalci <lkrbalci@gmail.com>
2023-08-24 14:49:24 +0300

    client: add hero section

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -5,6 +5,11 @@ import classes from "./Home.module.css";
 const Home = () => {
   return (
     <>
+      <section className={`${classes["hero"]} flex`}>
+        <h1>Buy?</h1>
+        <h1>Sell?</h1>
+        <h1>Your Journey, Our Expertise!</h1>
+      </section>
       <section className={classes["search"]}>
         <QuickSearch />
       </section>
--- client/src/pages/atomic/QuickSearch.tsx
+++ client/src/pages/atomic/QuickSearch.tsx
@@ -28,25 +28,32 @@ const QuickSearch = () => {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className={`${classes["quick-search"]} grid`}
-    >
-      <select {...register("brand")}>
-        <BrandSelectOptions />
-      </select>
-      <input {...register("series")} type="text" placeholder="Series" />
-      <input type="text" placeholder="Maximum Year" {...register("maxYear")} />
-      <input
-        type="text"
-        placeholder="Maximum Price"
-        {...register("maxPrice")}
-      />
-      <div className={`${classes["search-buttons"]} grid`}>
-        <Button isSubmit={true}>Search</Button>
-        <Link to="/search">Detailed Search</Link>
-      </div>
-    </form>
+    <div className={`${classes["quick-search-wrapper"]} flex`}>
+      <h1>Find Your Dream Car</h1>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className={`${classes["quick-search"]} grid`}
+      >
+        <select {...register("brand")}>
+          <BrandSelectOptions />
+        </select>
+        <input {...register("series")} type="text" placeholder="Series" />
+        <input
+          type="text"
+          placeholder="Maximum Year"
+          {...register("maxYear")}
+        />
+        <input
+          type="text"
+          placeholder="Maximum Price"
+          {...register("maxPrice")}
+        />
+        <div className={`${classes["search-buttons"]} grid`}>
+          <Button isSubmit={true}>Search</Button>
+          <Link to="/search">Detailed Search</Link>
+        </div>
+      </form>
+    </div>
   );
 };
 
