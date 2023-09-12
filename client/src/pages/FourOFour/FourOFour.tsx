dc1831169df4e829c05994ce0097c006cef384d8
lkrbalci <lkrbalci@gmail.com>
2023-09-12 16:12:07 +0300

    add 404 auto navigate

--- client/src/pages/FourOFour/FourOFour.tsx
+++ client/src/pages/FourOFour/FourOFour.tsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import classes from "./FourOFour.module.css";
+import { Link, useNavigate } from "react-router-dom";
 
 const fourOFour = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    let timeToWait = 5000;
+    const timer = setTimeout(() => {
+      navigate("/");
+    }, timeToWait);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
     <div className={`${classes["four-o-four-wrapper"]} flex`}>
       <div className={`${classes["four-o-four-text"]} flex`}>
@@ -11,6 +25,7 @@ const fourOFour = () => {
           Looks like this page took a wrong turn at the crossroads of the
           digital highway!
         </h2>
+        <Link to={"/"}>Would You Like to Go Home?</Link>
       </div>
     </div>
   );
